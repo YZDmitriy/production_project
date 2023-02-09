82babdc75420c0a1ab435f57ffbca1beebf128ea
Dmitriy <dyuzef@mail.ru>
2023-02-09 12:30:32 +0300

    config decomposition. build options

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from './types/config';
+
+export function buildPlugins(
+  options: BuildOptions
+): webpack.WebpackPluginInstance[] {
+  
+  const { paths } = options;
+
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
+}
