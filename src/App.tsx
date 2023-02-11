d57de850c3fc477785e0382ed99e04757127d3d5
dmitriy <dmitriy_yz@dmitriys-MacBook-Pro.local>
2023-02-11 16:46:58 +0300

    style organization. add themes light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React, { Suspense } from 'react';
-import './styles/index.scss';
-import { Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import React, { Suspense, useContext, useState } from "react";
+import "./styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
-export const App = () => {
+
+const App = () => {
+
+  const {theme, toggleTheme} = useTheme()
+ 
+
   return (
-    <div className='app light'>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+    <div className={`app ${theme} `}>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <button onClick={toggleTheme}>Toggle theme</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/'} element={<MainPageAsync />} />
-          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
         </Routes>
       </Suspense>
     </div>
   );
 };
+
+export default App;
