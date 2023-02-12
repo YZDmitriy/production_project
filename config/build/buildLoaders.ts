9bc6e956ec215d3e1c96fafcea2f8eed4fbbbb12
Dmitriy <dyuzef@mail.ru>
2023-02-12 19:03:15 +0300

    svg loader file loader button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,39 +1,55 @@
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/config';
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDev } = options;
+
+  // для понимания webpack файлов svg png  и др....
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
+
+  // для понимания webpack файлов svg png  и др....
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
 
   // для понимания webpack ом файлов SCSS
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
             localIdentName: isDev
-              ? '[path][name]__[local]--[hash:base64:5]'
-              : '[hash:base64:8]',
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
           },
         },
       },
       // Compiles Sass to CSS
-      'sass-loader',
+      "sass-loader",
     ],
   };
 
   // Если не используем TS нужен babel-loader !!!
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: 'ts-loader',
+    use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader];
+  return [typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
