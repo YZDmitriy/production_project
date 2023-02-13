bb5bc13b9c72f0c797a42a6aac607b2ec9e68ad9
Dmitriy <dyuzef@mail.ru>
2023-02-13 21:24:22 +0300

    Sidebar. add togle for sidebar  Layout app

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -5,18 +5,18 @@ import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
-  console.log(routeConfig);
-
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {Object.values(routeConfig).map(({ element, path }) => (
-            <Route key={path} path={path} element={element} />
-          ))}
-        </Routes>
-      </Suspense>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<div className="page-wrapper">{element}</div>}
+          />
+        ))}
+      </Routes>
+    </Suspense>
   );
 };
 
