000b2859eeb832d4420486c65c4fb176e00a8983
Dmitriy <dyuzef@mail.ru>
2023-02-24 17:19:28 +0300

    fix ts

--- src/shared/ui/Modal/ui/Modal.stories.tsx
+++ src/shared/ui/Modal/ui/Modal.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Theme } from 'app/providers/ThemeProvider';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+// import { Theme } from 'app/providers/ThemeProvider';
+// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Modal } from './Modal';
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
