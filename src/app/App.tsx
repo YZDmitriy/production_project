14a0c931ebc050e93228db794f396993021374c7
Dmitriy <dyuzef@mail.ru>
2023-03-14 21:15:54 +0300

    Router v6 private protectеd routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,14 @@ import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 const App = () => {
   const dispatch = useDispatch();
+  const inited = useSelector(getUserInited);
+
+  console.log(inited)
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
@@ -19,7 +22,7 @@ const App = () => {
         <Navbar />
         <div className="content-page">
           <Sidebar />
-          <AppRouter />
+          {inited && <AppRouter />}
         </div>
       </Suspense>
     </div>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,6 +1,7 @@
 import { UserSchema, User } from './model/types/user';
 import { userActions, userReducer } from './model/slice/userSlice';
 import { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+import { getUserInited } from './model/selectors/getUserInited/getUserInited';
 
 export {
   userActions,
@@ -8,4 +9,5 @@ export {
   type User,
   type UserSchema,
   getUserAuthData,
+  getUserInited,
 };
