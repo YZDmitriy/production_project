067850761faeb2f05b3ca3f9f7e1f13878acd930
Dmitriy <dyuzef@mail.ru>
2023-03-16 10:40:29 +0300

    Entity article, async thunk, slices. Blocks. Skeleton loader

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -9,9 +9,19 @@ import {
 import cls from './ArticleDetails.module.scss';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { fetchArticleById } from '../../model/services/fetchArticleById/fetchArticleById';
+import { useSelector } from 'react-redux';
+import {
+  getArticleDetailsData,
+  getArticleDetailsError,
+  getArticleDetailsIsLoading,
+} from '../../model/selectors/articleDetails';
+import { Text, TextAlign, TextTheme } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { Loader } from 'shared/ui/Loader';
 
 interface ArticleDetailsProps {
   className?: string;
+  id: string;
 }
 
 const reducers: ReducersList = {
@@ -19,18 +29,54 @@ const reducers: ReducersList = {
 };
 
 export const ArticleDetails = memo((props: ArticleDetailsProps) => {
-  const { className } = props;
+  const { className, id } = props;
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
 
+  // const isLoading = useSelector(getArticleDetailsIsLoading);
+  const isLoading = true;
+  const article = useSelector(getArticleDetailsData);
+  const error = useSelector(getArticleDetailsError);
+
   useEffect(() => {
-    dispatch(fetchArticleById('1'));
-  }, [dispatch]);
+    dispatch(fetchArticleById(id));
+  }, [dispatch, id]);
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div>
+        <>
+          <Skeleton
+            className={cls.avatar}
+            width={200}
+            height={200}
+            border="50%"
+          />
+          <Skeleton className={cls.title} width={300} height={32} />
+          <Skeleton className={cls.skeleton} width={600} height={24} />
+          <Skeleton className={cls.skeleton} width="100%" height={200} />
+          <Skeleton className={cls.skeleton} width="100%" height={200} />
+        </>
+      </div>
+    );
+  } else if (error) {
+    content = (
+      <Text
+        align={TextAlign.CENTER}
+        title={t('Произошла ошибка при загрузке')}
+        theme={TextTheme.ERROR}
+      />
+    );
+  } else {
+    content = <div>Article</div>;
+  }
 
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <div className={classNames(cls.articleDetails, {}, [className])}>
-        {t('ArticleDetails')}
+        {content}
       </div>
     </DynamicModuleLoader>
   );
