801b594865ddea8ca33767613a5ef3c26a17499c
Dmitriy <dyuzef@mail.ru>
2023-03-19 11:42:15 +0300

    skip loki for ArticleDetails.stories

--- src/shared/ui/Skeleton/Skeleton.stories.tsx
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -1,39 +1,39 @@
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
-// import { Theme } from 'app/providers/ThemeProvider';
-// import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { Skeleton } from './Skeleton';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Skeleton } from './Skeleton';
 
-// // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-// export default {
-//   title: 'shared/Skeleton',
-//   component: Skeleton,
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
-// } as ComponentMeta<typeof Skeleton>;
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: 'shared/Skeleton',
+  component: Skeleton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Skeleton>;
 
-// const Template: ComponentStory<typeof Skeleton> = (args) => (
-//   <Skeleton {...args} />
-// );
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <Skeleton {...args} />
+);
 
-// export const Normal = Template.bind({});
-// Normal.args = {};
+export const Normal = Template.bind({});
+Normal.args = {};
 
-// export const Circle = Template.bind({});
-// Circle.args = {
-//   border: '50%',
-//   width: 100,
-//   height: 100,
-// };
+export const Circle = Template.bind({});
+Circle.args = {
+  border: '50%',
+  width: 100,
+  height: 100,
+};
 
-// export const NormalDark = Template.bind({});
-// NormalDark.args = {};
-// NormalDark.decorators = [ThemeDecorator(Theme.DARK)];
+export const NormalDark = Template.bind({});
+NormalDark.args = {};
+NormalDark.decorators = [ThemeDecorator(Theme.DARK)];
 
-// export const CircleDark = Template.bind({});
-// CircleDark.args = {
-//   border: '50%',
-//   width: 100,
-//   height: 100,
-// };
-// CircleDark.decorators = [ThemeDecorator(Theme.DARK)];
+export const CircleDark = Template.bind({});
+CircleDark.args = {
+  border: '50%',
+  width: 100,
+  height: 100,
+};
+CircleDark.decorators = [ThemeDecorator(Theme.DARK)];
