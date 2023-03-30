28f20631d255585c6706c793202f26dc5c900150
Dmitriy <dyuzef@mail.ru>
2023-03-30 21:12:51 +0300

    pre_commit start production versel

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -2,11 +2,5 @@ import { FC, lazy } from 'react';
 import { AddCommentFormProps } from './AddCommentForm';
 
 export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(
-  async () =>
-    await new Promise((resolve) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      setTimeout(() => {
-        resolve(import('./AddCommentForm'));
-      }, 1500);
-    })
+  async () => await import('./AddCommentForm')
 );
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -1,10 +1,5 @@
 import { lazy } from 'react';
 
 export const ArticleDetailsPageAsync = lazy(
-  async () =>
-    await new Promise((resolve) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      setTimeout(() => { resolve(import('./ArticleDetailsPage')); }, 400);
-    })
+  async () => await import('./ArticleDetailsPage')
 );
