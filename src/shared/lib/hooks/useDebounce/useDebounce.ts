6caecb1a63bfb52426669813ac541cdea605c417
Dmitriy <dyuzef@mail.ru>
2023-04-25 10:52:58 +0300

    Project documentation

--- src/shared/lib/hooks/useDebounce/useDebounce.ts
+++ src/shared/lib/hooks/useDebounce/useDebounce.ts
@@ -1,5 +1,10 @@
 import { useRef, useCallback, MutableRefObject } from 'react';
 
+/**
+ * Хук, который позволяет отменять предыдущий вызов функции пока не истечет delay
+ * @param callback
+ * @param delay - задержка в мс
+ */
 export function useDebounce(callback: (...args: any[]) => void, delay: number) {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   const timer = useRef() as MutableRefObject<any>;
