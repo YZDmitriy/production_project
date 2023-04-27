f5b8340a85a725c28d7e67568aaa93652dfc754b
Dmitriy <dyuzef@mail.ru>
2023-04-27 20:57:19 +0300

    Improving the build of Babel loader. cache Postinstall hooks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -13,23 +13,20 @@ export function buildPlugins(
 ): webpack.WebpackPluginInstance[] {
   const { paths, isDev, apiUrl, project } = options;
 
+  const isProd = !isDev;
+
   const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
+
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
     }),
-    new CopyPlugin({
-      patterns: [{ from: paths.locales, to: paths.buildLocales }],
-    }),
+
     new CircularDependencyPlugin({
       exclude: /node_modules/,
       failOnError: true,
@@ -51,6 +48,21 @@ export function buildPlugins(
     plugins.push(
       new BundleAnalyzerPlugin({
         openAnalyzer: false,
+      })
+    );
+  }
+
+  // плагины будут добавляться только в продакшен сборку
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      })
+    );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: paths.locales, to: paths.buildLocales }],
       })
     );
   }
