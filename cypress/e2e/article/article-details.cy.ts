2dbf8858bf202abca933593e88927c8af28afbe0
Dmitriy <dyuzef@mail.ru>
2023-05-02 20:55:16 +0300

    Moki. Stubs. Fixtures. Skip tests. Interceptors for requests and fixtures

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -1,34 +1,41 @@
 let currentArticleId = '';
 describe('Пользователь заходит на страницу статьи', () => {
-    beforeEach(() => {
-        // Залогинились
-        cy.login();
-        // Создали статью для теста
-        cy.createArticle().then((article) => {
-            currentArticleId = article.id;
-            cy.visit(`articles/${article.id}`);
-        });
+  beforeEach(() => {
+    // Залогинились
+    cy.login();
+    // Создали статью для теста
+    cy.createArticle().then((article) => {
+      currentArticleId = article.id;
+      cy.visit(`articles/${article.id}`);
     });
-    afterEach(() => {
-        // Удалили статью, что бы не засорять БД
-        cy.removeArticle(currentArticleId);
-    });
-    it('И видит содержимое статьи', () => {
-        cy.getByTestId('ArticleDetails.Info').should('exist');
-    });
-    it('И видит список рекомендаций', () => {
-        cy.getByTestId('ArticleRecommendationsList').should('exist');
-    });
-    it('И оставляет комментарий', () => {
-        cy.getByTestId('ArticleDetails.Info');
-        cy.getByTestId('AddCommentForm').scrollIntoView();
-        cy.addComment('text');
-        cy.getByTestId('CommentCard.Content').should('have.length', 1);
-    });
-    it('И ставит оценку', () => {
-        cy.getByTestId('ArticleDetails.Info');
-        cy.getByTestId('RatingCard').scrollIntoView();
-        cy.setRate(4, 'feedback');
-        cy.get('[data-selected=true]').should('have.length', 4);
-    });
+  });
+  afterEach(() => {
+    // Удалили статью, что бы не засорять БД
+    cy.removeArticle(currentArticleId);
+  });
+  it('И видит содержимое статьи', () => {
+    cy.getByTestId('ArticleDetails.Info').should('exist');
+  });
+  it('И видит список рекомендаций', () => {
+    cy.getByTestId('ArticleRecommendationsList').should('exist');
+  });
+  it('И оставляет комментарий', () => {
+    cy.getByTestId('ArticleDetails.Info');
+    cy.getByTestId('AddCommentForm').scrollIntoView();
+    cy.addComment('text');
+    cy.getByTestId('CommentCard.Content').should('have.length', 1);
+  });
+  it('И ставит оценку', () => {
+    cy.getByTestId('ArticleDetails.Info');
+    cy.getByTestId('RatingCard').scrollIntoView();
+    cy.setRate(4, 'feedback');
+    cy.get('[data-selected=true]').should('have.length', 4);
+  });
+  it('И ставит оценку (пример с стабом на фикстурах)', () => {
+    cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
+    cy.getByTestId('ArticleDetails.Info');
+    cy.getByTestId('RatingCard').scrollIntoView();
+    cy.setRate(4, 'feedback');
+    cy.get('[data-selected=true]').should('have.length', 4);
+  });
 });
