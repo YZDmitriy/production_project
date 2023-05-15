ca46b2aad96711289c8917f34fda7cd27778fb28
Dmitriy <dyuzef@mail.ru>
2023-05-15 20:48:15 +0300

    SVGR. Processing the sizes and colors of the icon at the assembly stage

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,7 +9,24 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   // для понимания webpack файлов svg png  и др....
   const svgLoader = {
     test: /\.svg$/,
-    use: ['@svgr/webpack'],
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'convertColors',
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
   };
 
   // для понимания webpack файлов svg png  и др....
--- src/shared/lib/features/setGetFeatures.ts
+++ src/shared/lib/features/setGetFeatures.ts
@@ -2,7 +2,7 @@ import { FeatureFlags } from '@/shared/types/featureFlags';
 
 // ФИЧИ НЕ МЕНЯЮТСЯ В ХОДЕ СЕССИИ, ИХ НЕ ОБЯЗАТЕЛЬНО ДЕЛАТЬ РЕАКТИВНЫМИ!
 // РЕАКТИВНЫЕ ФИЧИ МЕНЯЮТСЯ
-let featureFlags: FeatureFlags;
+let featureFlags: FeatureFlags = {};
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
   if (newFeatureFlags) {
@@ -10,13 +10,6 @@ export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
   }
 }
 
-// export function getFeatureFlags(flag: keyof FeatureFlags) {
-//   return featureFlags[flag];
-// }
-
 export function getFeatureFlags(flag: keyof FeatureFlags) {
-  if (!featureFlags) {
-    return false;
-  }
   return featureFlags[flag];
 }
