ac5c717b17a20b5a8bebb8a0ab00f794d1e54675
Dmitriy <dyuzef@mail.ru>
2023-05-25 09:51:57 +0300

    Widget with additional information about the article. i18n plural forms

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -2,22 +2,22 @@ import { HTMLAttributeAnchorTarget, memo } from 'react';
 import { ArticleView } from '../../model/consts/articleConsts';
 import { Article } from '../../model/types/article';
 import { ToggleFeatures } from '@/shared/lib/features';
-// import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
 import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
 
 export interface ArticleListItemProps {
-    className?: string;
-    article: Article;
-    view: ArticleView;
-    target?: HTMLAttributeAnchorTarget;
+  className?: string;
+  article: Article;
+  view: ArticleView;
+  target?: HTMLAttributeAnchorTarget;
 }
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
-    return (
-        <ToggleFeatures
-            feature="isAppRedesigned"
-            on={<ArticleListItemRedesigned {...props} />}
-            off={<div>12345</div>}
-        />
-    );
+  return (
+    <ToggleFeatures
+      feature="isAppRedesigned"
+      on={<ArticleListItemRedesigned {...props} />}
+      off={<ArticleListItemDeprecated {...props} />}
+    />
+  );
 });
