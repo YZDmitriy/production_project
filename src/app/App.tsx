43bf1bddf3fb6b387f0ca845acb6ad0d7d28232e
Dmitriy <dyuzef@mail.ru>
2023-05-31 12:59:36 +0300

    Toolbar as part of the Layout Scroll Toolbar. use current route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,11 +11,14 @@ import { PageLoader } from '@/widgets/PageLoader';
 import { ToggleFeatures } from '@/shared/lib/features';
 import { MainLayout } from '@/shared/layouts/MainLayout';
 import { AppLoaderLayout } from '@/shared/layouts/AppLoaderLayout';
+import { useAppToolbar } from './lib/useAppToolbar';
 
 function App() {
   const { theme } = useTheme();
   const dispatch = useAppDispatch();
   const inited = useSelector(getUserInited);
+
+  const toolbar = useAppToolbar();
 
   useEffect(() => {
     if (!inited) {
@@ -58,6 +61,7 @@ function App() {
               header={<Navbar />}
               content={<AppRouter />}
               sidebar={<Sidebar />}
+              toolbar={toolbar}
             />
           </Suspense>
         </div>
