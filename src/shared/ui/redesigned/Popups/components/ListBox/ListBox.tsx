aaf3f61107b7d9321ac320c2e96f18e282338392
Dmitriy <dyuzef@mail.ru>
2023-06-06 22:01:43 +0300

    last commit

--- src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
+++ src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
@@ -55,7 +55,7 @@ export function ListBox<T extends string>(props: ListBoxProps<T>) {
         value={value}
         onChange={onChange}
       >
-        <HListBox.Button className={cls.trigger}>
+        <HListBox.Button as="div" className={cls.trigger}>
           <Button
             variant="filled"
             disabled={readonly}
